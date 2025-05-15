767fac9257189ebaa21aaef7e413c21e82bb2320
Paul Smurthwaite <psmurthw@outlook.com>
2025-05-15 19:56:23 +0100

    Update directory structure and scaffold

---
+++ src/bash/ap-dhcp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# List DHCP IP leases of previously connected clients
+sudo cat /var/lib/misc/dnsmasq.leases
---
+++ src/bash/ap-stations.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run with a hostapd AP running:
+sudo hostapd_cli -i wlx00c0cab4b58c all_sta  # list connected clients
